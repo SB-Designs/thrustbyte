d88584b8bac59e33194c88c92332cbe6dbbffc1e
SB Designs <hello@sbdesigns.spike.team>
2025-06-18 20:31:56 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ export default function Header({ name }) {
       <Link href="/">
         <a className="block w-[800px] h-auto mx-auto mb-6" aria-label={`${name} home`}>
           <Image
-            src="https://trackle.pages.dev/assets/logo.png // 👈 Replace with your image path
+            src="https://trackle.pages.dev/assets/logo.png" // 👈 Replace with your image path
             alt={`${name} logo`}
             width={800}
             height={800} // Adjust height for your logo aspect ratio
