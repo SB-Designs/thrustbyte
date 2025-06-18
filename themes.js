d7335ec572ff395774bf1b6aa04b8ec5c19d5746
SB Designs <hello@sbdesigns.spike.team>
2025-06-18 19:28:52 +0100

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,11 +1,11 @@
 exports.COLOR_THEMES = {
   default: {
     colors: {
-      primary: '#7D7AFF',
-      'gradient-1': '#7d7aff',
-      'gradient-2': '#2121e2',
-      'gradient-3': '#00fff0',
-      'gradient-4': '#8785FF',
+      primary: '#1f003d',
+      'gradient-1': '#2d0554',
+      'gradient-2': '#3b096d',
+      'gradient-3': '#501090',
+      'gradient-4': '#621aaa',
     },
   },
   bejamas: {
