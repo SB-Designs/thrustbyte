e2d85d760454fba15636beab2adb4992868ae80f
SB Designs <hello@sbdesigns.spike.team>
2025-06-18 20:17:38 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,13 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <Link href="/">
-        <a className="block w-12 h-12 mx-auto mb-4 rounded-full overflow-hidden relative" aria-label={`${name} home`}>
+        <a className="block w-[800px] h-auto mx-auto mb-6" aria-label={`${name} home`}>
           <Image
-            src="https://s1.qwant.com/thumbr/474x474/9/f/f06b292353af9e001f0415365f61f368118484107b5205dae3992bb0d80c79/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.XiBfzPtRlG99W70e7sdnHwHaHa%26pid%3DApi&q=0&b=1&p=0&a=0" // 👈 replace with your image path or URL
+            src="https://trackle.pages.dev/assets/logo.png // 👈 Replace with your image path
             alt={`${name} logo`}
-            layout="fill" // makes the image fill the container
-            className="object-cover"
+            width={800}
+            height={800} // Adjust height for your logo aspect ratio
+            className="object-contain"
           />
         </a>
       </Link>
