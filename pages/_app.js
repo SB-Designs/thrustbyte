d4d958197af1682bca605755313b6ed8a3689e4f
SB Designs <hello@sbdesigns.spike.team>
2025-06-19 07:12:36 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+        <title>Thrustbyte</title> {/* Optional: Set default title */}
+      </Head>
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
