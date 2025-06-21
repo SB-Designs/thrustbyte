62d4506e592908935acc534f809286f0d8630c6e
SB Designs <hello@sbdesigns.spike.team>
2025-06-21 18:55:57 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -17,7 +17,7 @@ export function GradientBackground({ variant, className }) {
 export default function Layout({ children }) {
   const setAppTheme = () => {
     const darkMode = localStorage.getItem('theme') === 'dark';
-    const lightMode = localStorage.getItem('theme') === 'light';
+    const darkMode = localStorage.getItem('theme') === 'light';
 
     if (darkMode) {
       document.documentElement.classList.add('dark');
